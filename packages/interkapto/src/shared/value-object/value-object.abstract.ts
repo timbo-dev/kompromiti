a76a8832db01ef62ea7368ed841dfbf67798a738
timbo-dev <dev@nathanrocha.com.br>
2023-08-13 20:11:51 -0300

    ✨ feat(interkapto/value-object): add AbstractValueObject class
    Implemented the AbstractValueObject abstract class, providing methods for creating, validating, and checking the legitimacy of value objects.
    
    The AbstractValueObject abstract class has been introduced to serve as a foundation for creating and managing value objects within the project. This class defines the following methods:
    
    create(value: any): Either<any, any>: This method is responsible for creating a value object using the provided value. It returns an Either type representing the error (err) or the success value.
    
    isLegit<T>(value: T): boolean: This method checks the legitimacy of a value object by validating it using the validate method and determining if the validation result is successful (Ok).
    
    validate(value: any): Either<Exception, any>: This method validates a value object. It returns an Either type, representing the error (err) or the success value.
    
    This abstract class promotes a standardized approach to managing value objects, enabling consistent creation, validation, and legitimacy checks throughout the project.

---
+++ packages/interkapto/src/shared/value-object/value-object.abstract.ts
@@ -0,0 +1,18 @@
+import { Either } from '@shared/either/either.type';
+import err from '@shared/either/err';
+import Exception from '@shared/exception/exception';
+
+export default abstract class AbstractValueObject {
+    public static create(value: any): Either<any, any> {
+        return err(value);
+    }
+
+    public static isLegit<T>(value: T): boolean {
+        const validationResult = AbstractValueObject.validate(value);
+        return validationResult.isOk();
+    }
+
+    public static validate(value: any): Either<Exception, any> {
+        return err(value);
+    }
+}
