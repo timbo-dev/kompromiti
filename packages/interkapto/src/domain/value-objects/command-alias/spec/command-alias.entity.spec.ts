01ee7323f5996be50de620171038c06a79d30fa8
timbo-dev <dev@nathanrocha.com.br>
2023-08-21 22:56:25 -0300

    🧪 test(interkapto/command-alias): add command alias creation test case
    Introduce a test case for command alias creation
    
    This test case verifies the creation of a command alias using the `CommandAlias` value object entity. It confirms that the `create` method correctly creates an instance of `CommandAlias` and ensures the assigned alias matches the provided value. This contributes to comprehensive testing for the `CommandAlias` value object entity.

---
+++ packages/interkapto/src/domain/value-objects/command-alias/spec/command-alias.entity.spec.ts
@@ -0,0 +1,12 @@
+describe('command-alias value object entity tests', () => {
+    it('should create a command alias', () => {
+        const sut = CommandAlias.create('a');
+
+        expect(sut.isOk()).toBe(true);
+        expect(sut.getValue()).toBeInstanceOf(CommandAlias);
+
+        const sutCommandAlias = (sut.getValue() as CommandAlias);
+
+        expect(sutCommandAlias.getAlias()).toBe('a');
+    });
+});
