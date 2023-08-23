0c4369e7e57edf036495526703267a4b436931ff
timbo-dev <dev@nathanrocha.com.br>
2023-08-23 14:52:07 -0300

    🧪 test(interkapto/command-flag): add creation test case
    The provided test case adds coverage for creating a command flag using the CommandFlag value object. It verifies that the creation process works as expected, ensuring that a valid CommandFlag instance is created with the specified flag name and flag value. The test also checks that the created flag's name and value match the provided values.

---
+++ packages/interkapto/src/domain/value-objects/command-flag/spec/command-flag.entity.spec.ts
@@ -0,0 +1,16 @@
+describe('command-flag value object entity tests', () => {
+    it('should create a command flag', () => {
+        const sut = CommandFlag.create({
+            flagName: 'force',
+            flagValue: 'true'
+        });
+
+        expect(sut.isOk()).toBe(true);
+        expect(sut.getValue()).toBeInstanceOf(CommandFlag);
+
+        const sutCommandFlag = (sut.getValue() as CommandFlag<boolean>);
+
+        expect(sutCommandFlag.getFlagName()).toBe('force');
+        expect(sutCommandFlag.getFlagValue()).toBe(true);
+    });
+});
