797f29144f5eefdd63cec98330c4c107aea65ec1
timbo-dev <dev@nathanrocha.com.br>
2023-08-25 12:39:50 -0300

    ✨ feat(interkapto/command-description): add missing exception type
    I've added the missing `EmptyStringCommandDescriptionException` to the `CommandDescriptionExceptions` type.

--- packages/interkapto/src/domain/value-objects/command-description/interfaces/command-description.types.ts
+++ packages/interkapto/src/domain/value-objects/command-description/interfaces/command-description.types.ts
@@ -1,4 +1,6 @@
+import EmptyStringCommandDescriptionException from '../exceptions/empty-string-command-description.exception';
 import NonStringCommandDescriptionException from '../exceptions/non-string-command-description.exception';
 
 export type CommandDescriptionExceptions =
     | NonStringCommandDescriptionException
+    | EmptyStringCommandDescriptionException
