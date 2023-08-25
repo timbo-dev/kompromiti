11beae258170c97b59401b80a783bf2f6a5de329
timbo-dev <dev@nathanrocha.com.br>
2023-08-25 12:59:20 -0300

    ✨ feat(interkapto/command-flag): add missing exception types
    I've added the missing exception types to the CommandFlagExceptions union in the command-flag.types.ts file.

--- packages/interkapto/src/domain/value-objects/command-flag/interfaces/command-flag.types.ts
+++ packages/interkapto/src/domain/value-objects/command-flag/interfaces/command-flag.types.ts
@@ -1,7 +1,15 @@
-import Exception from '@shared/exception/exception';
+import EmptyStringCommandFlagException from '../exceptions/empty-string-command-flag.exception';
+import NonStringCommandFlagValueException from '../exceptions/non-string-command-flag-value.exception';
+import NonStringCommandFlagException from '../exceptions/non-string-command-flag.exception';
+import NotParsableCommandFlagValueException from '../exceptions/not-parseable-command-flag-value.exception';
+import WhitespaceCommandFlagException from '../exceptions/whitespace-command-flag.exception';
 
 export type CommandFlagExceptions =
-    | Exception
+    | EmptyStringCommandFlagException
+    | NonStringCommandFlagValueException
+    | NonStringCommandFlagException
+    | NotParsableCommandFlagValueException
+    | WhitespaceCommandFlagException
 
 export interface CommandFlagDTO {
     flagName: string;
